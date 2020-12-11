2220cb416d9e65669e7ee75d2fd9bba03a2a3328
ichwanshihab <73604234+ichwanshihab@users.noreply.github.com>
2020-12-11 15:22:12 +0700

    dark mode-full page

--- js/dark.js
+++ js/dark.js
@@ -1,6 +1,15 @@
-const checkbox = 
-document.getElementById('checkbox');
+let toggler = document.querySelector(".toggler");
+let aboutUs = document.querySelector(".about-us-wrapper");
+let product = document.querySelector(".products-wrapper");
+let aboutImg = document.querySelector(".about-us-image");
+let productsImg = document.querySelector(".products");
+let contact = document.querySelector(".contact-us");
 
-checkbox.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
-});
+toggler.addEventListener('click', () => {
+	aboutUs.classList.toggle("light");
+	aboutImg.classList.toggle("light");
+	product.classList.toggle("light");
+	productsImg.classList.toggle("light");
+	contact.classList.toggle("light");
+	document.body.classList.toggle('light');
+})
